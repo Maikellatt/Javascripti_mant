6294ff50dec0bdd532eae7c41bef822e3b0f2922
Maikel Lätt <maikellatt@MacBook-Air.local>
2024-12-04 17:17:32 +0200

    DOM õpetamine juurde

--- Algtõed/functions.js
+++ Algtõed/functions.js
@@ -4,7 +4,7 @@
  * Funktsioon võtan sulgude vahele parameetrid (placeholder)
  */
 function sum(num1, num2) {
-    return num1 + num2,
+    return num1 + num2
 }
 
 const summaryOfTwoNumbers = sum(4, 6);
@@ -16,5 +16,16 @@ const summaryOfTwoNumbers = sum(4, 6);
 function fullName(firstName, lastName) {
     //return firstname + '' + lastName;
     return `${firstName} ${lastName}` ;
+}
 
     console.log(fullName("Maikel", "Lätt")) ;
+
+    /**
+     * IIFE-funktsioon
+     * 
+     */
+    
+    (() => {
+        console.log("IIFE")
+
+})();
