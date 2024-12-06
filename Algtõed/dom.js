a687d61df3104571e2dc1fb185341d5972c90097
Maikel Lätt <maikellatt@MacBook-Air.local>
2024-12-06 15:26:55 +0200

    kuni yl14 tehtud

--- Algtõed/dom.js
+++ Algtõed/dom.js
@@ -11,16 +11,36 @@ h1element .innerText = "Head Teed!"
 const fruits = ["Apple", "Orange", "Banana", "Mango", "Kiwi"];
 
 const fruitListCointainer = document.querySelector('#fruit-list');
-const orderedListElement = document.createElement('ol');
 
-for(let i = 0; i < fruits.length; i++) {
-    const listElement = document.createElement('li');
-    listElement.innerText = fruits[i];
-    orderedListElement.appendChild(listElement);
 
-}
+function renderList() {
+    const orderedListElement = document.createElement('ol');
+    fruitListCointainer.innerHTML = "";
+    
+    for(let i = 0; i < fruits.length; i++) {
+        const listElement = document.createElement('li');
 
-fruitListCointainer.appendChild(orderedListElement);
+        const spanElement = document.createElement('span');
+        listElement.innerText = fruits[i];
+
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "🗑️";
+        deleteButton.classList.add('delete-btn');
+        deleteButton.setAttribute('data-index', i); 
+
+
+        listElement.appendChild(spanElement);
+        listElement.appendChild(deleteButton);
+
+        orderedListElement.appendChild(listElement);
+
+    }
+    fruitListCointainer.appendChild(orderedListElement);
+};
+
+renderList();
+
+
 
 
 const dontTouchMeButton = document.querySelector('#dont-touch-me-button');
@@ -28,4 +48,35 @@ const dontTouchMeButton = document.querySelector('#dont-touch-me-button');
 dontTouchMeButton.addEventListener('click', (event) => {
     console.log(event);
     alert("Ei tohtinud ju");
+});
+
+const addNewFruitForm = document.querySelector('#add-new-fruits-form');
+addNewFruitForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const fruitInput = formData.get('fruit');
+    console.log(fruitInput);
+
+    if(!fruitInput.trim()) return;
+
+    fruits.push(fruitInput);
+    renderList();
+
+    event.target.reset();
+    
+
+
+});
+
+// Kustutame elemente
+
+fruitListCointainer.addEventListener('click', (event) => {
+    const buttonElement = event.target.closest('.delete-btn');
+    const index = parseInt(buttonElement.getAttribute("data-index"));
+
+    // TODO imploment check for index
+        fruits.splice(index, 1);
+         renderList()   
+   
 });
